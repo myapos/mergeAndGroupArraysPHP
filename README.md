df5dcc03d5e58300ed149d4b9242022bfab92b6d
myapos <myapos@yahoo.com>
2017-03-11 21:25:17 +0200

    description and documentation

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # mergeArrayList
+
+Description:
+Function mergeArrayListByProPerty($dataMetaV,'WebpaceId','Views','Clicks');
+This function is useful in data grouping. 
+
+---------Structure Of Data-----------
+$dataMeta1 = [
+
+ [
+ 'Views' => 1,
+ 'Clicks' => 5,
+ 'Myros'  => 10,
+ 'WebpaceId' => 3061,
+ 'SiteInfo' => [
+                'Metadescr'=>'MetaAction1',
+                 'siteId' => '855'
+              ]
+ ],
+  [
+ 'Views' => 1,
+ 'Clicks' => 5,
+ 'Myros'  => 10,
+ 'WebpaceId' => 3061,
+ 'SiteInfo' => [
+                'Metadescr'=>'MetaAction1',
+                 'siteId' => '860'
+              ]
+ ],
+ .
+ .
+ .
+
+
+ ]
+---------Arguments-----------
+
+$dataMeta : The data to be ordered
+$propertyToGroup: This property will be used in order to scan the array list, 
+group and add the values  with common values of data structure. Has to be present in all data structure
+$property1ToAdd, $property2ToAdd: these properties are the sums of all common values
+ 
--- functions.php
+++ functions.php
@@ -1,12 +1,48 @@
 <?php 
 /*
 Description:
+This function is useful in data grouping. 
 
+---------Structure Of Data-----------
+$dataMeta1 = [
+
+ [
+ 'Views' => 1,
+ 'Clicks' => 5,
+ 'Myros'  => 10,
+ 'WebpaceId' => 3061,
+ 'SiteInfo' => [
+                'Metadescr'=>'MetaAction1',
+                 'siteId' => '855'
+              ]
+ ],
+  [
+ 'Views' => 1,
+ 'Clicks' => 5,
+ 'Myros'  => 10,
+ 'WebpaceId' => 3061,
+ 'SiteInfo' => [
+                'Metadescr'=>'MetaAction1',
+                 'siteId' => '860'
+              ]
+ ],
+ .
+ .
+ .
+
+
+ ]
+---------Arguments-----------
+
+$dataMeta : The data to be ordered
+$propertyToGroup: This property will be used in order to scan the array list, 
+group and add the values  with common values of data structure. Has to be present in all data structure
+$property1ToAdd, $property2ToAdd: these properties are the sums of all common values
 
 */
 
 function mergeArrayListByProPerty($dataMeta, $propertyToGroup, $property1ToAdd, $property2ToAdd ) {
-//function mergeArrayListByProPerty($dataMeta, $propertyToGroup, $properties ) {
+
     $printFlag = 1;
     $printJson = 0;
     if($printFlag){
